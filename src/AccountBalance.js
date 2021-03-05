0195beab3c58977746b44ebd4829f0a5112407de
Chan Qijian <Chan_Qijian@artc.a-star.edu.sg>
2021-03-05 12:35:45 +0800

    Create routing

--- src/AccountBalance.js
+++ src/AccountBalance.js
@@ -15,17 +15,18 @@ function AccountBalance() {
     async function getInfo() {
         try {
             await fetch("https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance", {
-                method: "POST",
-                headers: { "x-api-key": "FagLlQytW3aPBTWJXcAxo2QA1QqEtr2u3xnBPLAd"},
-                body: JSON.stringify(balanceAccount)
-            }).then(response => {
-                response.json();
-
-            })
+              method: "POST",
+              headers: { "x-api-key": "FagLlQytW3aPBTWJXcAxo2QA1QqEtr2u3xnBPLAd"},
+              body: JSON.stringify(balanceAccount)
+            }).then(response => response.json())
             .then(data => {
                 setAssetBalance(data.assetBalance);
                 setCashBalance(data.cashBalance)
             });
+
+
+            //const parseRes = await response.json();
+            //setName(parseRes.profile_name);
           } catch (err) {
             console.error(err.message);
           }
--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import AccountBalance from "./AccountBalance"
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <AccountBalance />
+      <Switch>
+          <Route path="/" component={AccountBalance} exact/>
+      </Switch>
       </header>
     </div>
   );
