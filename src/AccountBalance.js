28af5b76a39c4bcea594800046d596e23f521083
Chan Qijian <Chan_Qijian@artc.a-star.edu.sg>
2021-03-05 12:17:38 +0800

    Account balance page creation

---
+++ src/AccountBalance.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from "react";
+
+function AccountBalance() {
+    let apikey = "FagLlQytW3aPBTWJXcAxo2QA1QqEtr2u3xnBPLAd";
+    let loginInfo = {
+        "username": "Group5",
+        "password": "sI5_BY_IaMTdS_9"
+    };
+    let balanceAccount = {
+        "accountKey": "adf0c561-9bef-4bb9-ae63-f6d755f254a0"
+    }
+    const [assetBalance, setAssetBalance] = useState(0);
+    const [cashBalance, setCashBalance] = useState(0);
+
+    async function getInfo() {
+        try {
+            await fetch("https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance", {
+                method: "POST",
+                headers: { "x-api-key": "FagLlQytW3aPBTWJXcAxo2QA1QqEtr2u3xnBPLAd"},
+                body: JSON.stringify(balanceAccount)
+            }).then(response => {
+                response.json();
+
+            })
+            .then(data => {
+                setAssetBalance(data.assetBalance);
+                setCashBalance(data.cashBalance)
+            });
+          } catch (err) {
+            console.error(err.message);
+          }
+    }
+
+    useEffect(() => {
+        getInfo();
+      }, []);
+
+    return(
+        <div>
+            AccountID: {balanceAccount["accountKey"]}
+            <br />
+            Asset Balance: {assetBalance}
+            <br />
+            Cash Balance:{cashBalance}
+        </div>);
+}
+
+export default AccountBalance;
