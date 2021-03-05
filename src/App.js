8f76b9107578754f1b7b936c71847665a536006b
bosco.kor <boscokor40@gmail.com>
2021-03-05 12:00:31 +0800

    loginpage
    loginpage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React, { useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+import Login from './components/login/login';
+
+
+
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="App">
       <header className="App-header">
